39daf5856bfe71489fff4eef863fe1f292ff4e80
KevinLong <long.luong.contact@gmail.com>
2025-12-23 18:41:10 +0700

    update JS

--- script.js
+++ script.js
@@ -59,27 +59,75 @@ countdownTimer();
 
 // Flash Sales Slider ------------------------------------
 
+const flashSlider = document.querySelector(".flash-slider");
 const flashSaleTrack = document.querySelector(".flash-track");
-const cards = document.querySelectorAll(".product");
-const btnLeft = document.querySelector(".flash-arrow--left");
-const btnRight = document.querySelector(".flash-arrow--right");
+const items = Array.from(document.querySelectorAll(".product"));
+const btnPrev = document.querySelector(".flash-arrow--left");
+const btnNext = document.querySelector(".flash-arrow--right");
 
-let translateIndex = 0;
-const itemsPerSlide = 4;
+function getItemMetrics() {
+  const item = items[0];
+  const itemRect = item.getBoundingClientRect();
 
-function updateFlashSaleSlider() {
-  flashSaleTrack.style.transform = `translateX(-${
-    translateIndex * (250 + 20)
-  }px)`;
+  const styles = getComputedStyle(flashSaleTrack);
+  const gap = parseFloat(styles.columnGap || styles.gap || 0);
+
+  return {
+    itemWidth: itemRect.width,
+    gap,
+    step: itemRect.width + gap,
+  };
 }
 
-btnRight.addEventListener("click", () => {
-  if (translateIndex < cards.length - itemsPerSlide)
-    translateIndex += itemsPerSlide;
-  updateFlashSaleSlider();
+function getVisibleCount() {
+  const sliderWidth = flashSlider.getBoundingClientRect().width;
+  const { step } = getItemMetrics();
+
+  return Math.floor(sliderWidth / step);
+}
+
+let currentIndex = 0;
+
+function updateFlashSlider() {
+  const { step } = getItemMetrics();
+  flashSaleTrack.style.transform = `translateX(-${currentIndex * step}px)`;
+}
+btnNext.addEventListener("click", () => {
+  const visible = getVisibleCount();
+  const maxIndex = items.length - visible;
+
+  currentIndex = Math.min(currentIndex + visible, maxIndex);
+  updateFlashSlider();
 });
 
-btnLeft.addEventListener("click", () => {
-  if (translateIndex >= itemsPerSlide) translateIndex -= itemsPerSlide;
-  updateFlashSaleSlider();
+btnPrev.addEventListener("click", () => {
+  const visible = getVisibleCount();
+
+  currentIndex = Math.max(currentIndex - visible, 0);
+  updateFlashSlider();
 });
+
+// let translateIndex = 0;
+// const itemsPerSlide = 4;
+
+// function updateFlashSaleSlider() {
+//   flashSaleTrack.style.transform = `translateX(-${
+//     translateIndex * (260 + 20) // chiều rộng product-item + padding-inline
+//   }px)`;
+// }
+
+// btnRight.addEventListener("click", () => {
+//   if (translateIndex < cards.length - itemsPerSlide)
+//     translateIndex += itemsPerSlide;
+//   updateFlashSaleSlider();
+// });
+
+// btnLeft.addEventListener("click", () => {
+//   if (translateIndex >= itemsPerSlide) translateIndex -= itemsPerSlide;
+//   updateFlashSaleSlider();
+// });
+// --------------------------------------- TEST
+
+const a = getItemMetrics();
+
+console.log(a);
